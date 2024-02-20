f2eb20a876058e894deecf88df7bcfe466f22c66
Sumit Soni <122284205+sonisumit7904@users.noreply.github.com>
2024-02-21 00:17:02 +0530

    Update mission1.py

--- mission1.py
+++ mission1.py
@@ -1,33 +1,43 @@
-#!/usr/bin/env python
 #######################################################################
-# Mission Description
-#
-# We want to calculate a sum of squares of some integers, excepting negatives
-# * The first line of the input will be an integer N (1 <= N <= 100)
-# * Each of the following N test cases consists of one line containing an integer X (0 < X <= 100),
-#   followed by X integers (Yn, -100 <= Yn <= 100) space-separated on the next line
-# * For each test case, calculate the sum of squares of the integers excepting negatives,
-#   and print the calculated sum to the output. No blank line between test cases
-# * (Take input from standard input, and output to standard output)
-# 
-# ##Rules
-# * Choose your favorite language from either of these: C, Erlang, Go, Python, Ruby
-# * Do not use loop statements like while/until/for/each/loop, and goto
-#
-# ## Sample Input
+# Mission 1: Write a program which fulfills the requirements below
+# Description
+# We want you to calculate the sum of squares of given integers, excluding any negatives.
+# The first line of the input will be an integer N (1 <= N <= 100), indicating the number of test cases to follow.
+# Each of the test cases will consist of a line with an integer X (0 < X <= 100), followed by another line consisting of X number of space-separated integers Yn (-100 <= Yn <= 100).
+# For each test case, calculate the sum of squares of the integers, excluding any negatives, and print the calculated sum in the output.
+# Note: There should be no output until all the input has been received.
+# Note 2: Do not put blank lines between test cases solutions.
+# Note 3: Take input from standard input, and output to standard output.
+# Rules
+# Write your solution using Go Programming Language or Python Programming Language. Do not submit your solution with both languages at once!
+
+# You may only use standard library packages. In addition, extra point is awarded if solution does not declare any global variables.
+
+# Specific rules for Python solution
+# Your source code must be a single file, containing at least a main function
+# Do not use any for loop, while loop, or any list / set / dictionary comprehension
+# Your solution will be tested against Python 3.11 (as of February 2023) or higher
+# def main():
+#     ...
+
+# if __name__ == "__main__":
+#     main()
+
+# Sample Input
 # 2
 # 4
 # 3 -1 1 14
 # 5
 # 9 6 -53 32 16
-#
-# ##Sample Output
+
+# Sample Output
 # 206
 # 1397
 #######################################################################
 
 import sys
 
+# NOT A GLOBAL VARIABLE, ONLY FUNCTION
 # map function
 square_map = lambda x : x*x
 sum_map = lambda x: x
@@ -73,6 +83,9 @@ def calculate(times, map_func = sum_map, filter_fun = pos_filter):
         sys.stderr.write("at %d input dataset\n" % (times))
     return calculate(times-1, map_func, filter_fun)
 
-n = get_iteration_times()
-calculate(n, square_map, pos_filter)
+def main():
+  n = get_iteration_times()
+  calculate(n, square_map, pos_filter)
 
+if __name__ == "__main__":
+  main()
